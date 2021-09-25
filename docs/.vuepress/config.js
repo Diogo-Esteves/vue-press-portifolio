2ae962a35b154fc98de62d82d31e816710cd672c
Diogo Esteves dos Santos <diogoesteves_skt@hotmail.com>
2021-09-25 13:02:08 -0300

    restarting the project, new commit, with new theme and nothing more

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    lang: 'en-US',
+    title: 'Diogo Portifolio!',
+    description: 'This is my first VuePress site',
+    theme: 'vuepress-theme-maker',
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png'
+      // Please head documentation to see the available options.
+    }
+  }
